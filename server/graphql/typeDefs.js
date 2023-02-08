3d909c90dc83e19190d40011c47b77e400db977f
Pablo Carro <pablo.carro@live.com>
2023-02-08 17:49:12 -0300

    task defs

--- server/graphql/typeDefs.js
+++ server/graphql/typeDefs.js
@@ -4,16 +4,27 @@ import gql from "graphql-tag";
 export const typeDefs = gql`
     type Query {
         hello: String
+        projects: [Project]
+        task: [Task]
     }
 
     type Mutation {
         createProject(name: String, description: String): Project
+        createTask(title: String, projectId: ID): Task
     }
 
     type Project {
         _id: ID
         name: String
         description: String
+        createdAt: String
+        updatedAt: String
+    }
+
+    type Task {
+        _id: ID
+        title: String
+        projectId: ID
         createdAt: String
         updatedAt: String
     }
