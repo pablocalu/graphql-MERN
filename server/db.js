8213a141aa5329de57a0cd93411eb7b4900c8507
Pablo Carro <pablo.carro@live.com>
2023-02-12 22:41:06 -0300

    backend fix

--- server/db.js
+++ server/db.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
-import { MONGODB_URI } from "./config";
+import { MONGODB_URI } from "./config.js";
 
 export const connectDB = async () => {
     try {
+        mongoose.set('strictQuery', false)
         const conn = await mongoose.connect(MONGODB_URI)
         console.log(`MongoDB connected: ${conn.connection.name}`)
     } catch (error) {
