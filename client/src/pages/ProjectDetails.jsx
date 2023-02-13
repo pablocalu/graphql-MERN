a3e629409f48dd7d1b1ac36bc6b2160e9c175e73
Pablo Carro <pablo.carro@live.com>
2023-02-13 15:02:58 -0300

    tasklist

--- client/src/pages/ProjectDetails.jsx
+++ client/src/pages/ProjectDetails.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import { useQuery } from '@apollo/client'
 import { GET_PROJECT } from '../graphql/projects'
+import TaskList from '../components/tasks/TaskList'
 
 export default function ProjectDetails() {
 
@@ -9,12 +10,18 @@ export default function ProjectDetails() {
   const {data, loading, error} = useQuery(GET_PROJECT, {
     variables: {
       id: params.id
-    }
+    },
+    skip: !params.id //la primera vez q se ejecuta no obtiene el params entonces busca undefined, con este skip lo saltea la primera vez y desp fx con normaldiad
   })
 
+  if(loading) return <p>Loading...</p>
+  if(error) return <p>Error {error}...</p>
+
   return (
-    <div>
-      <h1>Projects Details</h1>
-    </div>
+    <>
+      <h1>{data.project.name}</h1>
+      <p>{data.project.description}</p>
+      <button>Delete</button>
+    </>
   )
 }
