8b6cf9bddc99531babcf42d4207069436f246b51
Pablo Carro <pablo.carro@live.com>
2023-02-12 23:50:36 -0300

    gql mutation

--- client/src/components/ProjectForm.jsx
+++ client/src/components/ProjectForm.jsx
@@ -1,9 +1,11 @@
+
 import React, { useState } from 'react'
+import { useMutation } from '@apollo/client'
 
 export default function ProjectForm() {
 
   const [project, setProject ] = useState({
-    title: '',
+    name: '',
     description: ''
   })
 
@@ -13,9 +15,13 @@ export default function ProjectForm() {
       [e.target.name]: e.target.value
     })
   }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+  }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input type="text" name='name' placeholder='Write a title' onChange={handleChange} />
       <textarea name='description' rows="3" placeholder='Write a description' onChange={handleChange} ></textarea>
       <button>
