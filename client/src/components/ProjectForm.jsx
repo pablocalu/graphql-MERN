a60b09e77bd6590b3a6d53064c8139eeb3de81ba
Pablo Carro <pablo.carro@live.com>
2023-02-12 23:46:47 -0300

    form

--- client/src/components/ProjectForm.jsx
+++ client/src/components/ProjectForm.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function ProjectForm() {
+
+  const [project, setProject ] = useState({
+    title: '',
+    description: ''
+  })
+
+  const handleChange = (e) => {
+    setProject({
+      ...project,
+      [e.target.name]: e.target.value
+    })
+  }
+
   return (
-    <div>
-      
-    </div>
+    <form>
+      <input type="text" name='name' placeholder='Write a title' />
+      <textarea name='description' rows="3" placeholder='Write a description' ></textarea>
+      <button>
+        Submit
+      </button>
+    </form>
   )
 }
